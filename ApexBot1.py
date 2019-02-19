f17fd6f99b5c6db7a68077b533ef65b0f06b644d
Zirke <defrenefre@hotmail.com>
2019-02-19 12:29:56 +0100

    Restructure with new api, working commands .kills/damage/wins 'name'

--- ApexBot1.py
+++ ApexBot1.py
@@ -1,125 +1,108 @@
 #!python3.6
-import re
 import asyncio
 import json
 import requests
 import discord
-import sched
-import time
 from discord.ext import commands
 
 TOKEN = 'MzI1MjY5OTMwNDUyNzEzNDgy.D0r9VA.EyQTiWXoQJQ-yzt11tu0OxTPGCI'
 
-client = commands.Bot(command_prefix = '.')
+client = commands.Bot(command_prefix='.')
+
 
 def inf_from_tracker(name):
-	url = 'https://public-api.tracker.gg/apex/v1/standard/profile/5/'+name
-	headers = {'TRN-Api-Key': '8b3b6e28-122f-4d9a-8c54-794fca417953'}
-	r = requests.get(url, headers=headers)
-	return r.text
+    url = 'https://public-api.tracker.gg/apex/v1/standard/profile/5/' + name
+    headers = {'TRN-Api-Key': '8b3b6e28-122f-4d9a-8c54-794fca417953'}
+    r = requests.get(url, headers=headers)
+    #print(r.text)
+    return r.text
+
 
 @client.event
 async def on_ready():
-	print('Bot online')
+    print('Bot online')
+
+
+def query_api(name, stat):
+    value = 0
+    output = ''
+    try:
+        data = json.loads(inf_from_tracker(name))
+    except ValueError:
+        print("Invalid json")
+    else:
+        for i in range(0, len(data['data']['children'][0]['stats'])):
+            if data['data']['children'][0]['stats'][i]['metadata']['key'] == stat:
+                print(data['data']['children'][0]['stats'][i]['metadata']['key'])
+                value = data['data']['children'][0]['stats'][i]['value']
+        output = name.capitalize() + ' has got a total of : ' + str(int(value)) + ' ' + stat.capitalize() + '!'
+    return output
+
 
 @client.command()
-async def update(*args):
-	errorMsg = None
-	lst=[]
-	if len(args) is not 2:
-		for word in args:
-			name = word
-		if not name:
-			name = 'Dritix'
-		stat = 'kills'
-	else:
-		for word in args:
-			lst.append(word)
-		name = lst[0]
-		if lst[1] == 'kills' or lst[1] == 'Kills':
-			statMsg = 'kills'
-			stat = 'kills'
-		elif lst[1] == 'wins' or lst[1] == 'Wins':
-			statMsg = 'wins'
-			stat = 'winsWithFullSquad'
-		elif lst[1] == 'damage' or lst[1] == 'Damage':
-			statMsg = 'damage'
-			stat = 'damage'
-		else:
-			stat = 'kills'
-			errorMsg = 'Syntax error: only kills, wins or damage is supported'
+async def kills(*args):
+    stat = 'Kills'
+    errormsg = None
+    if len(args) is not 1:
+        errormsg = 'Syntax error: Use synatx .kills name'
+    else:
+        for word in args:
+            name = word
+    await client.say(query_api(name, stat))
 
-	value = []
-	legend = 0
-	legentlst = []
-	output = ""
+@client.command()
+async def damage(*args):
+    stat = 'Damage'
+    errormsg = None
+    if len(args) is not 1:
+        errormsg = 'Syntax error: Use synatx .damage name'
+    else:
+        for word in args:
+            name = word
+    await client.say(query_api(name, stat))
 
-	try:
-		data = json.loads(inf_from_tracker(name))
-	except ValueError:
-		print ("invalid json")
-	else:
-		for i in range(0, len(data)):
-			if 'value' in data[i][stat]:
-				value.append(data[i][stat]['value'])
-			if 'legend' in data[i]:
-				if 'value' in data[i][stat]:
-					legend = data[i]['legend']
-					if legend == 1:
-						legentlst.append("Wraith")
-					elif legend == 2:
-						legentlst.append("Bangalore")
-					elif legend == 3:
-						legentlst.append("Caustic")
-					elif legend == 4:
-						legentlst.append("Mirage")
-					elif legend == 5:
-						legentlst.append("Bloodhound")
-					elif legend == 6:
-						legentlst.append("Gibraltar")
-					elif legend == 7:
-						legentlst.append("Lifeline")
-					elif legend == 8:        
-						legentlst.append("Pathfinder")
-	if len(args) is not 2: 
-		output = 'Could not understand, but '+name+' has '+str(value)+' kills! (Use syntax .update name kills/wins/damage)'
-	elif errorMsg is not None:
-		output = errorMsg + ', but '+name+' has '+str(value)+' kills! (Use syntax .update name kills/wins/damage)'
-	else:
-		output = name + " has got a total of:\n"
-		for v, l, in zip(value, legentlst):
-			output += (str(v) + ' ' + statMsg + ' with ' + str(l) + '\n')
-		#output = name+' has got a total of : '+str(value)+ ' ' + statMsg + ' with ' + ' and '.join(legentlst)
-	await client.say(output)
+@client.command()
+async def wins(*args):
+    stat = 'WinsWithFullSquad'
+    errormsg = None
+    if len(args) is not 1:
+        errormsg = 'Syntax error: Use synatx .wins name'
+    else:
+        for word in args:
+            name = word
+    await client.say(query_api(name, stat))
+
 
 def broadcastWins(name):
-	value = 0
-	try:
-		data = json.loads(inf_from_tracker(name))
-	except ValueError:
-		print ("invalid json")
-	else:
-		for i in range(0, len(data)):
-			if 'value' in data[i]['winsWithFullSquad']:
-				#if value < data[i]['kills']['value']:
-				value += data[i]['winsWithFullSquad']['value']
-	return int(value)
+    value = 0
+    try:
+        data = json.loads(inf_from_tracker(name))
+    except ValueError:
+        print("invalid json")
+    else:
+        for i in range(0, len(data)):
+            if 'value' in data[i]['winsWithFullSquad']:
+                # if value < data[i]['kills']['value']:
+                value += data[i]['winsWithFullSquad']['value']
+    return int(value)
+
 
 async def background_task_wins(name):
-	wins = {}
-	newWins = {}
-	await client.wait_until_ready()
-	channel = discord.Object(id='325296935667761152')
-	wins[name] = broadcastWins(name)
-	while not client.is_closed:
-		newWins[name] = broadcastWins(name)
-		if newWins[name] > wins[name]:
-			output = name+' has just won '+str(newWins[name]-wins[name])+' game(s)!'
-			wins[name] = newWins[name]
-			await client.send_message(channel, output)
-		await asyncio.sleep(5)
+    wins = {}
+    newWins = {}
+    await client.wait_until_ready()
+    channel = discord.Object(id='325296935667761152')
+    wins[name] = broadcastWins(name)
+    while not client.is_closed:
+        newWins[name] = broadcastWins(name)
+        if newWins[name] > wins[name]:
+            output = name + ' has just won ' + str(newWins[name] - wins[name]) + ' game(s)!'
+            wins[name] = newWins[name]
+            await client.send_message(channel, output)
+        await asyncio.sleep(5)
 
-#for name in broadcastNames:
-	#client.loop.create_task(background_task_wins(name))
+
+# for name in broadcastNames:
+# client.loop.create_task(background_task_wins(name))
 
 client.run(TOKEN)
