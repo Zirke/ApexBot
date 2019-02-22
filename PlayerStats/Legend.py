47d0555bce30753f3c2a6f4904d24689da92d108
TheKaergaard <31920988+TheKaergaard@users.noreply.github.com>
2019-02-22 13:56:18 +0100

    update

--- PlayerStats/Legend.py
+++ PlayerStats/Legend.py
@@ -5,3 +5,7 @@ class Legend:
     def __init__(self, legendName, stats):
         self.legendName = legendName
         self.stats = stats
+
+    # Overwriting tostring
+    def __repr__(self):
+        return "Legend name: " + self.legendName + " Stat: " + str(self.stats)
--- PlayerStats/Player.py
+++ PlayerStats/Player.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 from PlayerStats.Legend import Legend
 from PlayerStats.Stat import Stat
@@ -20,7 +21,7 @@ def json_from_tracker(name):
     return r.json()
 
 
-def query_api(name):
+def query_api_data(name):
     try:
         data = json_from_tracker(name)
     except ValueError:
@@ -30,15 +31,58 @@ def query_api(name):
     return data
 
 
-def receive_legends(name):
-    data = query_api(name)
+def query_legend_id(name):
+    data = query_api_data(name)
+    legendIdList = []
+    for i in range(0, len(data['data']['children'])):
+        legendIdList.append(data['data']['children'][i]['id'])
+    return legendIdList
+
+
+def query_legend_stats(data, playerLegendList):
+    for i in playerLegendList:
+        if data['data']['children'][0]['id'] == i:
+            #print(json.dumps(data, indent=4))
+            if data['data']['children'][0]['stats'][0]['metadata']['key'] == 'Kills':
+                output = data['data']['children'][0]['stats'][0]['metadata']['value']
+                #print(json.dumps(data, indent=4))
+                print(output)
+
+
+
+def initialize_legends(legendIDs):
     legendlist = []
-    for i in range(0, len(data['data']['children'][0]['id'])):
-        legendlist.append(i)
+    stat = 0
+    for lid in legendIDs:
+        if lid == 'legend_1':
+            wraith = Legend("Wraith", stat)
+            legendlist.append(wraith)
+        elif lid == 'legend_2':
+            bangalore = Legend("Bangalore", stat)
+            legendlist.append(bangalore)
+        elif lid == 'legend_3':
+            caustic = Legend("Caustic", stat)
+            legendlist.append(caustic)
+        elif lid == 'legend_4':
+            mirage = Legend("Mirage", stat)
+            legendlist.append(mirage)
+        elif lid == 'legend_5':
+            bloodhound = Legend("Bloodhound", stat)
+            legendlist.append(bloodhound)
+        elif lid == 'legend_6':
+            gibraltar = Legend("Gibraltar", stat)
+            legendlist.append(gibraltar)
+        elif lid == 'legend_7':
+            lifeline = Legend("Lifeline", stat)
+            legendlist.append(lifeline)
+        elif lid == 'legend_8':
+            pathfinder = Legend("pathfinder", stat)
+            legendlist.append(pathfinder)
     return legendlist
 
 
-print(receive_legends('Dritix'))
-s1 = Stat("Kills", 434, 42342)
+# print(json.dumps(query_api_data('Dritix'), indent=4))
 
-leg1 = Legend("Mirage", s1)
+
+print(query_legend_stats(query_api_data('Dritix'), query_legend_id('Dritix')))
+# print(initialize_legends(query_legend_id('Dritix')))
